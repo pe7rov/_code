a95af30b3bf3991826fa203dc4f7000ba48e5d0c
Petrov Victor <vovalowara@gmail.com>
2025-06-03 18:11:21 +0800

    14 commit

--- script.js
+++ script.js
@@ -12,15 +12,10 @@ visionButton.addEventListener('click', () => {
     visionButton.setAttribute('aria-label', 'Выключить режим для слабовидящих');
     visionButton.textContent = 'Выключить режим для слабовидящих';
     
-    // Можно дополнительно увеличить шрифт через inline стиль или оставить только класс
-    // например, если хотите более точное управление:
-    // document.body.style.fontSize = '1.5em';
-    
+        // Восстановление стандартных настроек
 } else {
     visionButton.setAttribute('aria-label', 'Включить режим для слабовидящих');
     visionButton.textContent = 'Включить режим для слабовидящих';
     
-    // Восстановление стандартных настроек (если нужно)
-    // document.body.style.fontSize = '';
 }
 });
